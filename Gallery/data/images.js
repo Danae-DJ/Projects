ff636105213a2cff0125385025dab983d04f86a6
Danae-DJ <danae_taiyaki@hotmail.com>
2025-04-02 22:33:17 -0600

    added images-js

---
+++ Gallery/data/images.js
@@ -0,0 +1,32 @@
+const imagesData = [
+    {
+        title: "Park in Autumn",
+        description: "It's a beautiful cloudy day in autumn.",
+        src: "assets\images\park-3731094_1280.jpg",
+        category: "nature"
+    },
+    {
+        title: "Myself",
+        description: "I like taking pictures of myself.",
+        src: "assets\images\woman-2010387_1280.jpg",
+        category: "selfies"
+    },
+    {
+        title: "My pet, My love",
+        description: "My dog climbed the tree, it's very nice.",
+        src: "assets\images\pets-3389723_1280.jpg",
+        category: "nature"
+    },
+    {
+        title: "They are my friends",
+        description: "They are the cousin's family",
+        src: "assets\images\family-286229_1280.jpg",
+        category: "portraits"
+    },
+    {
+        title: "A little princess",
+        description: "She my cute friend; Ashley my neighbor",
+        src: "assets\images\kid-1241817_1280.jpg",
+        category: "selfies"
+    }
+]
