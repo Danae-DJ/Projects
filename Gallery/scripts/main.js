e5fb677de03743b9a88c75ec1c45ecd353a6bf34
Danae-DJ <danae_taiyaki@hotmail.com>
2025-04-03 20:31:06 -0600

    added css

--- Gallery/scripts/main.js
+++ Gallery/scripts/main.js
@@ -6,44 +6,45 @@ const modalTitle = document.getElementById('modal-title');
 const modalDesc = document.getElementById('modal-description');
 const closeModalBtn = document.getElementById('close-modal');
 
-//funtion cards images
+// cards-images
 function displayImages(category = "all") {
-    gallery.innerHTML = "";
+  gallery.innerHTML = ""; 
 
-    const filtered = category === "all"
-        ? imagesData
-        : imagesData.filter(img => img.category === category);
-    
-    filtered.forEach(img => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.innerHTML = `
-            <img src="${img.src}" alt="${img.title}">
-            <h3>${img.title}</h3>
-        `;
-        
-        card.addEventListener('click', () => {
-            modalImg.src = img.src;
-            modalTitle.textContent = img.title;
-            modalDesc.textContent = img.description;
-            modal.classList.remove('hidden');
-        });
+  const filtered = category === "all" 
+    ? imagesData 
+    : imagesData.filter(img => img.category === category);
 
-        gallery.appendChild(card);
+  filtered.forEach(img => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+      <img src="${img.src}" alt="${img.title}">
+      <h3>${img.title}</h3>
+    `;
+
+    card.addEventListener('click', () => {
+      modalImg.src = img.src;
+      modalTitle.textContent = img.title;
+      modalDesc.textContent = img.description;
+      modal.classList.remove('hidden');
     });
+
+    gallery.appendChild(card);
+  });
 }
 
-// funtion to filters
+// Filters
 filterButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const category = btn.dataset.category;
-        displayImages(category);
-    });
+  btn.addEventListener('click', () => {
+    const category = btn.dataset.category;
+    displayImages(category);
+  });
 });
 
-//close the modal
+// Close modal
 closeModalBtn.addEventListener('click', () => {
-    modal.classList.add('hidden');
+  modal.classList.add('hidden');
 });
 
+// All images
 displayImages();
